58f48c1ba3215af35a2a547f2469f87a2b9d54e7
nguyenhuynh2711 <22211tt2703@mail.tdc.edu.vn>
2025-04-25 23:05:22 +0700

    create funtion list user

---
+++ app/Http/Controllers/CrudUserController.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Support\Facades\DB;
+
+
+/**
+ * CRUD User controller
+ */
+class CrudUserController extends Controller
+{
+
+    /**
+     * Login page
+     */
+    public function login()
+    {
+        return view('crud_user.login');
+    }
+
+    /**
+     * User submit form login
+     */
+    public function authUser(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('list')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    /**
+     * Registration page
+     */
+    public function createUser()
+    {
+        return view('crud_user.create');
+    }
+
+    /**
+     * User submit form register
+     */
+    public function postUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+        $check = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+
+            'phone' => $data['phone'],
+            'gioitinh' => $data['gioitinh'],
+            'ngaysinh' => $data['ngaysinh'],
+
+
+            'password' => Hash::make($data['password'])
+        ]);
+
+        return redirect("login");
+    }
+
+    /**
+     * View user detail page
+     */
+    public function readUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.read', ['messi' => $user]);
+    }
+
+    /**
+     * Delete user by id
+     */
+    public function deleteUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::destroy($user_id);
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * Form update user page
+     */
+    public function updateUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.update', ['user' => $user]);
+    }
+
+    /**
+     * Submit form update user
+     */
+    public function postUpdateUser(Request $request)
+    {
+        $input = $request->all();
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,id,' . $input['id'],
+            'password' => 'required|min:6', // Cho phép password rỗng (nếu không muốn đổi mật khẩu)
+        ]);
+
+        $user = User::find($input['id']);
+        $user->name = $input['name'];
+        $user->email = $input['email'];
+
+        $user->phone = $input['phone'];
+        $user->gioitinh = $input['gioitinh'];
+        $user->ngaysinh = $input['ngaysinh'];
+
+
+        // Kiểm tra nếu password có nhập thì mới update, nếu không thì giữ nguyên mật khẩu cũ
+        if (!empty($input['password'])) {
+            $user->password = Hash::make($input['password']);
+        }
+
+        $user->save();
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * List of users
+     */
+    public function listUser()
+    {
+        return view('admin.list_users', [
+            'user' => optional(Auth::user())->load('roles'),
+            'users' => User::with('roles')->paginate(5)
+        ]);
+
+        // BỎ Auth::check() nếu muốn ai cũng truy cập được
+        // return view('admin.list_users', [
+        //     'users' => User::with('roles')->paginate(5)
+        // ]);
+
+
+        // if (Auth::check()) {
+        //     return view('admin.list_users', [
+        //         'users' => User::with('roles')->paginate(10)
+        //     ]);
+        // }
+
+        // return redirect("login")->withSuccess('You are not allowed to access');
+    }
+
+    /**
+     * Sign out
+     */
+    public function signOut()
+    {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
